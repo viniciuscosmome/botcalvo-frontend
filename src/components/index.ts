7967bb58ac2ed8c4a900453d105a6e7e5a22e56d
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-24 20:35:56 -0300

    chore: adds export files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,10 @@
 export * from './macro/Wrapper';
+export * from './macro/Dashboard';
 export * from './macro/HeaderDashboard';
 export * from './macro/ColumnDashboard';
+export * from './macro/NavigationSection';
 
 export * from './micro/ButtonAction';
 export * from './micro/Title';
+
+export * from './nano/HorizontalSplit';
