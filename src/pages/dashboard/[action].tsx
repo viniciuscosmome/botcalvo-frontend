48f1991d4639f12e9c5cee8743f41a23fc3a8aa5
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-24 20:36:34 -0300

    chore: updates dashboard preview

--- src/pages/dashboard/[action].tsx
+++ src/pages/dashboard/[action].tsx
@@ -1,21 +1,21 @@
-import { Wrapper, HeaderDashboard, ColumnDashboard } from '../../components';
+import { DashboardWrapper } from '../../components';
 
 export default function Dashboard() {
-  const user = {name: 'Ada Lovelace'};
+  const channels = [
+    { id: 123, name: 'canal 123', redirect: './channel&id=123' },
+    { id: 124, name: 'canal 124', redirect: './channel&id=124' },
+    { id: 125, name: 'canal 125', redirect: './channel&id=125' },
+  ];
+  const streams = [
+    { id: 'stream-id-1', name: 'stream 123', redirect: './stream&id=123' },
+    { id: 'stream-id-2', name: 'stream 124', redirect: './stream&id=124' },
+    { id: 'stream-id-3', name: 'stream 125', redirect: './stream&id=125' },
+    { id: 'stream-id-4', name: 'stream 126', redirect: './stream&id=126' },
+  ];
 
   return (
-    <Wrapper>
-      <HeaderDashboard user={user} />
-
-      <Wrapper nowrap workarea>
-        <ColumnDashboard select='left' workarea>
-          Workarea
-        </ColumnDashboard>
-
-        <ColumnDashboard select='right' workarea>
-          Navigation
-        </ColumnDashboard>
-      </Wrapper>
-    </Wrapper>
+    <DashboardWrapper channels={channels} streams={streams}>
+      Custom Dashboard
+    </DashboardWrapper>
   );
 }
