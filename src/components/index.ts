6815d92b4dda5d958f6ee1d7c7f4c4807b6b7655
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-24 00:28:21 -0300

    chore: exports columns, header and wrapper components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
+export * from './macro/Wrapper';
+export * from './macro/HeaderDashboard';
+export * from './macro/ColumnDashboard';
+
 export * from './micro/ButtonAction';
 export * from './micro/Title';
