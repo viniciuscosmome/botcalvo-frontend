fac80c3060451b628b6b0eb674a8f3d2e8884039
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-25 18:25:43 -0300

    feat: crates select theme component

---
+++ src/components/nano/SelectTheme/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import styles from './style.module.scss';
+
+type iThemeProp = {
+  data: string;
+  icon: string;
+  content: string;
+};
+
+export function SelectTheme() {
+  const [selectedTheme, setSelectedTheme] = useState(0);
+  const themes: Array<iThemeProp> = [
+    {
+      data: '',
+      icon: '',
+      content: 'padrão'
+    },
+    {
+      data: 'light',
+      icon: 'bi bi-brightness-high-fill',
+      content: 'claro'
+    },
+    {
+      data: 'dark',
+      icon: 'bi bi-moon-fill',
+      content: 'escuro'
+    }
+  ];
+
+  const setDocumentThemeAttribute = (index: number) => {
+    document.querySelector('html')?.setAttribute('data-user-theme', themes[index].data);
+    document.body.setAttribute('data-user-theme', themes[index].data);
+  };
+
+  const handleClick = (): void => {
+    console.log(selectedTheme);
+    let updateTo = selectedTheme + 1;
+    if (updateTo > 2) updateTo = 0;
+    setSelectedTheme(updateTo);
+    setDocumentThemeAttribute(updateTo);
+  };
+
+  return (
+    <button onClick={handleClick} className={styles.button}>
+      <span className={styles.circle}>
+        <i className={themes[selectedTheme].icon}></i>
+      </span>
+      <span className={styles.content}>
+        {themes[selectedTheme].content}
+      </span>
+    </button>
+  );
+}
