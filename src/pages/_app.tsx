98c98b6724539f7da74d7e076558c752eb2ceda5
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-26 18:24:09 -0300

    refactor: adds type when import types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
-import { ThemeContext, iThemeProps, themeDefaultValue } from '../components/macro/Context/';
+import { ThemeContext, themeDefaultValue } from '../components/macro/Context/';
+import type { iThemeProps } from '../components/macro/Context/';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
