df1b8212b9270033650970b94199a86c558a9186
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-26 17:06:17 -0300

    feat: adds theme context component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { ThemeContext, iThemeProps, themeDefaultValue } from '../components/macro/Context/';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState<iThemeProps | null>(themeDefaultValue);
+
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <Component {...pageProps} />
+    </ThemeContext.Provider>
+  );
 }
