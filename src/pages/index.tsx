519c2f4edbb049d1710ea27058c98ec39aaa1c92
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-11-26 21:23:48 -0300

    chore: updates home content selection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import { HomeWrapper, Inicio } from '../components';
 
 export default function Home() {
   const router = useRouter();
-  const { content } = router.query;
+  const {content} = router.query;
+  const options = ['home'];
 
-  const page = content ?? 'home';
+  const page = typeof content === 'string' && options.includes(content) ? content : 'home';
 
   return (
     <HomeWrapper>
