b6a7cb21f2afa2e6ede0d91dc4591ffba8b824d7
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-12-01 16:06:36 -0300

    refactor: remove white spaces

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { HomeWrapper, Inicio, LegalPrivacy, LegalUse } from '../components';
 
 export default function Home() {
   const router = useRouter();
-  const {content} = router.query;
+  const { content } = router.query;
   const options = ['home', 'legal-privacy', 'legal-use'];
 
   const page = typeof content === 'string' && options.includes(content) ? content : 'home';
