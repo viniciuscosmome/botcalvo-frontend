a00ee2e1c683d3b8bccc399e15afdec0bf34446f
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-12-02 16:10:53 -0300

    refactor: remove logout from listActions

--- src/pages/auth/[action].tsx
+++ src/pages/auth/[action].tsx
@@ -4,16 +4,16 @@ import { AuthenticationWrapper, Login, Register, Recovery, Reset } from '../../c
 export default function Authentication() {
   const route = useRouter();
   const { action } = route.query;
-  const listActions = ['login', 'logout', 'register', 'recovery', 'reset'];
+  const listActions = ['login', 'register', 'recovery', 'reset'];
 
   const page = typeof action === 'string' && listActions.includes(action) ? action : listActions[0];
 
   return (
     <AuthenticationWrapper>
       {page === listActions[0] && <Login />}
-      {page === listActions[2] && <Register />}
-      {page === listActions[3] && <Recovery />}
-      {page === listActions[4] && <Reset />}
+      {page === listActions[1] && <Register />}
+      {page === listActions[2] && <Recovery />}
+      {page === listActions[3] && <Reset />}
     </AuthenticationWrapper>
   );
 }
