1a7d3b7cbb366a37cfdedba915d9d8f37f6eee00
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-12-05 20:28:20 -0300

    feat: adds methods to load theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
-import { ThemeContext, themeDefaultValue } from '../components/mega/Context';
-import type { iThemeProps } from '../components/mega/Context';
+import ThemeContrroler from '../modules/theme';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<iThemeProps | null>(themeDefaultValue);
+  useEffect(ThemeContrroler.updateTheme, [ThemeContrroler.updateTheme]);
 
   return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
-      <Component {...pageProps} />
-    </ThemeContext.Provider>
+    <Component {...pageProps} />
   );
 }
