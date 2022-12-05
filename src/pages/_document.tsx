a77759ec4a118119a69837ded3fcd912af5abeb1
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-12-05 20:27:03 -0300

    chore: removes ThemeContext

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { useContext } from 'react';
-import { ThemeContext } from '../components/mega/Context';
 
 export default function Document() {
-  const context = useContext(ThemeContext);
-
   return (
     <Html lang={'pt-BR'}>
       <Head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
-      <body data-user-theme={context.theme?.data}>
+      <body data-user-theme="">
         <Main />
         <NextScript />
       </body>
