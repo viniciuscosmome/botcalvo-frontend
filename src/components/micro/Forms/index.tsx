a5bd9c2bbde39bb7b3f0c4697ada69e6595ff599
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-12-21 20:43:01 -0300

    chore: updates form component

--- src/components/micro/Forms/index.tsx
+++ src/components/micro/Forms/index.tsx
@@ -4,19 +4,19 @@ import styles from './style.module.scss';
 
 type iFormsProps = {
   customClass?: string;
-  action: string;
-  method: string;
+  action?: string;
+  method?: string;
   onsubmit: (event: SyntheticEvent) => void;
   children: React.ReactNode | Array<React.ReactNode>;
 };
 
-export function Forms({ customClass, action, method, onsubmit, children }: iFormsProps) {
+export function Forms({ customClass = '', action = '', method = 'POST', onsubmit, children }: iFormsProps) {
   return (
     <form
       action={action}
       method={method}
       onSubmit={onsubmit}
-      className={classes(styles.form, customClass ?? '')}>
+      className={classes(styles.form, customClass)}>
 
       {children}
     </form>
