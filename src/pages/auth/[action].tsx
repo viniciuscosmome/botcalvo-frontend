d312e402e3e09f3ab55646a55ff2dc7f37e00e77
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-12-23 11:11:29 -0300

    feat: add logout action

--- src/pages/auth/[action].tsx
+++ src/pages/auth/[action].tsx
@@ -3,12 +3,12 @@ import { useRouter } from 'next/router';
 
 import variables from '../../config/variables';
 import { validateToken } from '../../modules/Validates/geral';
-import { AuthenticationWrapper, Login, Register, Recovery, Reset } from '../../components';
+import { AuthenticationWrapper, Login, Logout, Register, Recovery, Reset } from '../../components';
 
 export default function Authentication() {
   const route = useRouter();
   const { action } = route.query;
-  const listActions = ['login', 'register', 'recovery', 'reset'];
+  const listActions = ['login', 'register', 'recovery', 'reset', 'logout'];
 
   const page = (action && typeof action === 'string' && listActions.includes(action)) ? action : listActions[0];
 
@@ -18,11 +18,19 @@ export default function Authentication() {
       {page === listActions[1] && <Register />}
       {page === listActions[2] && <Recovery />}
       {page === listActions[3] && <Reset />}
+      {page === listActions[4] && <Logout />}
     </AuthenticationWrapper>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const returnProps = {props: {}};
+
+  const action = context.params?.action;
+  const isLogoutRoute = (typeof action === 'string') && (action.trim().replace(/[^a-z]/i, '') === 'logout');
+
+  if (isLogoutRoute) return returnProps;
+
   const token = context.req.cookies[variables.cookie.token_name];
   const checkedToken = validateToken(token);
 
@@ -33,9 +41,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         permanent: false,
       }
     };
-  } else {
-    return {
-      props: {}
-    };
   }
+
+  return returnProps;
 };
